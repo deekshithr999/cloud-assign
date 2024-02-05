545b0550d21712e396cf86061df8b373b34223bf
Ubuntu <ubuntu@ip-172-31-44-76.us-east-2.compute.internal>
2024-02-05 04:47:34 +0000

    Cloud Assign

--- flaskapp/flaskapp.py
+++ flaskapp/flaskapp.py
@@ -2,23 +2,10 @@ import sqlite3
 from flask import Flask, render_template, g, request,redirect, url_for
 import os
 
-#PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
-
-#DATABASE = os.path.join(PROJECT_ROOT, 'flaskapp', 'tmpdb1.db')
-
 DATABASE = '/var/www/html/flaskapp/users.db'
 UPLOAD_FOLDER = 'uploads'
 app = Flask(__name__)
 app.config.from_object(__name__)
-
-#...
-
-
-#conn = sqlite3.connect('tmpdb1.db')
-#cur = conn.cursor()
-#cur.execute("""DROP TABLE IF EXISTS entries""")
-#cursor.execute('''CREATE TABLE IF NOT EXISTS your_table_name (id INTEGER PRIMARY KEY, name TEXT)''')
-
 
 
 def connect_to_database():
@@ -54,23 +41,13 @@ def mainpage():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    # Retrieve form data
     username = request.form['username']
     password = request.form['password']
     first_name = request.form['first_name']
     last_name = request.form['last_name']
     email = request.form['email']
 
-    #print("====================>", password, first_name)
-    # Store the information in the database
-    #conn = sqlite3.connect('user_info.db')
-    #cursor = conn.cursor()
-    #cursor.execute('''
-    #    INSERT INTO users (username, password, first_name, last_name, email)
-    #    VALUES (?, ?, ?, ?, ?)
-    #''', (username, password, first_name, last_name, email))
-    #conn.commit()
-    #conn.close()
+    print("====================>", password, first_name, username)
     CREATE_TABLE =  '''
                         CREATE TABLE IF NOT EXISTS users (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -85,7 +62,7 @@ def submit():
                                 INSERT INTO users (username, password, first_name, last_name, email)
                                 VALUES (?, ?, ?, ?, ?)
                         '''
-    CHECK_USER = ''' SELECT * FROM users WHERE username=? '''
+    CHECK_USER = ''' SELECT username, password  FROM users WHERE username=? '''
     execute_query(CREATE_TABLE)
     nfile = request.files['textfile']
     wc = None #default val
@@ -106,30 +83,21 @@ def submit():
 
 
     res = execute_query(CREATE_TABLE)
-    print("res from db create", res)
     commit()
     if username is None or password is None or first_name is None or last_name is None or email is None:
-        print("========================= here") 
         return render_template('mainpage.html', error='Enter all Details !!')
     res = execute_query(INSERT_INTO_TABLE, (username, password, first_name, last_name, email))
     print("res from db insert", res)
     commit()
-    # Redirect to a page displaying all details
     return redirect(url_for('display_details', username=username, password=password, wc=wc))
 
 
 @app.route('/display_details')
 def display_details():
-    # Retrieve information from the database based on the provided username and password
     username = request.args.get('username')
     password = request.args.get('password')
     wc = request.args.get('wc')
 
-    #conn = sqlite3.connect('user_info.db')
-    #cursor = conn.cursor()
-    #cursor.execute('SELECT * FROM users WHERE username=? AND password=?', (username, password))
-    #user_info = cursor.fetchone()
-    #conn.close()
     FETCH_DETAILS = '''
 			SELECT * FROM  users  WHERE  username=? AND password=?
 		    '''
